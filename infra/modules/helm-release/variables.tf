144d8a1d7dc0ba3d7eb107d15c719415eedd77a1
antoniomorenoborras <antonio.moreno@keepler.io>
2025-08-07 00:50:54 +0200

    chart variables

--- infra/modules/helm-release/variables.tf
+++ infra/modules/helm-release/variables.tf
@@ -1,18 +1,35 @@
-variable "namespace" {
-  default = ""
-}
-variable "chart_version" {
-  default = ""
-}
-variable "chart_name" {
-  default = ""
-}
-variable "chart_repository" {
-  default = ""
-}
 variable "release_name" {
-  default = ""
+  description = "Nombre del release de Helm."
+  type        = string
 }
+
+variable "chart_repository" {
+  description = "URL del repositorio del Helm chart."
+  type        = string
+}
+
+variable "chart_name" {
+  description = "Nombre del chart a instalar."
+  type        = string
+}
+
+variable "chart_version" {
+  description = "Versión del chart a instalar."
+  type        = string
+}
+
+variable "namespace" {
+  description = "Namespace de Kubernetes donde instalar el chart."
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Nombre del clúster EKS (usado para configurar el provider de Helm)."
+  type        = string
+}
+
 variable "values" {
-  default = ""
+  description = "Mapa de valores para pasar al Helm chart."
+  type        = map(any)
+  default     = {}
 }
