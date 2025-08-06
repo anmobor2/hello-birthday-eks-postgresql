c59d22775b111bbfe7421b9d9e0eaddf0af674d2
antoniomorenoborras <antonio.moreno@keepler.io>
2025-08-07 00:52:26 +0200

    provider helm

--- infra/modules/helm-release/main.tf
+++ infra/modules/helm-release/main.tf
@@ -1,3 +1,18 @@
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+  }
+}
+
+data "aws_eks_cluster" "main" {
+  name = var.cluster_name
+}
 
 resource "helm_release" "main" {
   name       = var.release_name
